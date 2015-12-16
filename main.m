26c6f349b1a741a942d62334b3f26af471c42894
SebastianoF <sebastiano.ferraris@gmail.com>
2015-12-16 13:47:46 +0000

    show comparison 1 finished

--- main.m
+++ main.m
@@ -12,15 +12,20 @@ clear
 close all
 clc
 
-format long
+%%%%%%%%%%%%%%%%%%
+%%% controller %%%
+%%%%%%%%%%%%%%%%%%
 
-% controller
-show_comparison_1 = true;
-show_comparison_2 = false;
+show_comparison_1 = false;
+show_comparison_2 = true;
 
-% handle exp methods
+% handles exp methods
 exp_method = {@exp_se2, @expm, @exp_leja, @expmv, @expmvp, @phileja, @phipm};
-exp_method_name = {'exp_se2', 'exp_leja', 'expmv', 'expmvp', 'phileja', 'phipm'};
+exp_method_name = {'expm', 'exp_leja', 'expmv', 'expmvp', 'phileja', 'phipm'};
+
+%%%%%%%%%%%%%%%%%%%
+%%% model- view %%%
+%%%%%%%%%%%%%%%%%%%
 
 if show_comparison_1 == true
 
@@ -34,6 +39,16 @@ if show_comparison_1 == true
     
     A = exp_se2(dA); 
     
+    A_v_gr      = A*v;
+    A_v_expm    = expm(dA)*v;
+    A_v_expleja = expleja(1, dA, v);
+    A_v_expmv   = expmv(1, dA, v, 'double');
+    A_v_expmvp  = expmvp(1, dA, v);
+    A_v_phileja = phileja(1, dA, 0, v);
+    A_v_phipm   = phipm(1, dA, v);
+    
+    format short
+    
     disp('Matrix in the algebra se2: dA = ')
     disp(dA)
     disp('Corresponding matrix in the group SE2: A = ')
@@ -42,22 +57,40 @@ if show_comparison_1 == true
     disp('Vector v')
     disp(v)
     disp('Product A times v, ground truth')
-    disp(A*v)
+    disp(A_v_gr)
     disp('Products expm(dA) * v')
     disp('With expm:')
-    disp(expm(dA)*v)
+    disp(A_v_expm)
     disp('With expleja:')
-    disp(expleja(1, dA, v))
+    disp(A_v_expleja)
     disp('With expmv:')
-    disp(expmv(1, dA, v, 'double'))
+    disp(A_v_expmv)
     disp('With expmvp:')
-    disp(expmvp(1, dA, v))
+    disp(A_v_expmvp)
     disp('With phileja:')
-    disp(phileja(1, dA, 1, v))
+    disp(A_v_phileja)
     disp('With phipm:')
-    disp(phipm(1, dA, v))
+    disp(A_v_phipm)
     
+    format long
     
+    % errors for each method:
+    errors = [norm(A_v_gr - A_v_expm    , 2),    ...
+              norm(A_v_gr - A_v_expleja , 2), ...
+              norm(A_v_gr - A_v_expmv   , 2),   ...
+              norm(A_v_gr - A_v_expmvp  , 2),  ...
+              norm(A_v_gr - A_v_phileja , 2), ...
+              norm(A_v_gr - A_v_phipm, 2)];
+   
+    fprintf('\n Error for each method: class 4 ground truth available:')
+          
+    fprintf('\n\n %17s %17s %17s %17s %17s %17s\n', ...
+            'expm', 'exp_leja', 'expmv', 'expmvp', 'phileja', 'phipm');
+        
+    fprintf(' %17.8f %17.8f %17.8f %17.8f %17.8f %17.8f \n',...
+            errors');
+        
+    format short
     
 end
 
@@ -66,13 +99,12 @@ if show_comparison_2 == true
     disp('Second comparison with multiple random matrices of any class.')
     disp('The ground truth is given by expm(dA)*v .')
     
+    
+    
 end
 
 
-
-disp('Second example with :')
-
-
+disp('END')
 
 
 
